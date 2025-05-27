bac94cf171ad76fae62319d8ef25d734a69a8691
Nitish145 <royalnitish21@gmail.com>
2025-05-27 18:17:01 +0530

    add fixed block allocator impl

---
+++ fixed-block-allocator/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+class FixedBlockAllocator
+{
+public:
+    FixedBlockAllocator(size_t blockSize, size_t blockCount)
+        : size(blockSize), count(blockCount), buffer(nullptr), freeList(nullptr)
+    {
+        buffer = new char[blockSize * blockCount];
+        freeList = reinterpret_cast<FreeBlock *>(buffer);
+
+        FreeBlock *block = freeList;
+        for (size_t i = 1; i < blockCount; i++)
+        {
+            block->next = reinterpret_cast<FreeBlock *>(buffer + i * blockSize);
+            block = block->next;
+        }
+        block->next = nullptr;
+    }
+
+    void *allocate()
+    {
+        if (!freeList)
+            throw std::bad_alloc();
+
+        void *block = freeList;
+        freeList = freeList->next;
+        return block;
+    }
+
+    void deallocate(void *ptr)
+    {
+        FreeBlock *block = reinterpret_cast<FreeBlock *>(ptr);
+        block->next = freeList;
+        freeList = block;
+    }
+
+    ~FixedBlockAllocator()
+    {
+        delete[] buffer;
+    }
+
+private:
+    struct FreeBlock
+    {
+        FreeBlock *next;
+    };
+    size_t size;
+    size_t count;
+    char *buffer;
+    // the address of each free block is the address of memory
+    // which is available for allocation
+    FreeBlock *freeList;
+};
+
+int main()
+{
+    FixedBlockAllocator fixedAllocator(64, 10);
+
+    void *block1 = fixedAllocator.allocate();
+    void *block2 = fixedAllocator.allocate();
+    void *block3 = fixedAllocator.allocate();
+
+    std::cout << "Allocated blocks at: " << block1 << ", " << block2 << ", " << block3 << std::endl;
+
+    fixedAllocator.deallocate(block2);
+    std::cout << "Deallocated block at: " << block2 << std::endl;
+
+    void *block4 = fixedAllocator.allocate();
+    std::cout << "Allocated block at: " << block4 << " (should be same as deallocated block2)" << std::endl;
+
+    fixedAllocator.deallocate(block1);
+    fixedAllocator.deallocate(block3);
+    fixedAllocator.deallocate(block4);
+
+    std::cout << "All allocated blocks deallocated." << std::endl;
+}
